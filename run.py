9a1760f8f7b11287f98570f132d97b8a34b5939d
SebastianLoef <sebastian@loef.se>
2020-12-08 12:54:53 +0100

    Lade till några funktioner.

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from collections import namedtuple
 
 G = 6.67384e-11
+dt = 1
 
 Pos = namedtuple('Pos', ['x', 'y'])
 Hastighet = namedtuple('Hastighet', ['x', 'y'])
+Kraft = namedtuple('Kraft' ['x', 'y'])
 
 
 class Himlakropp():
@@ -16,6 +18,26 @@ class Himlakropp():
 
 
 def F(kropp1, kropp2):
-    r2 = (kropp2.pos.x - kropp1.pos.x)**2 +\
-         (kropp2.pos.y - kropp1.pos.y)**2
-    return G*kropp1.massa*kropp2.massa/r2
+    dx = kropp1.pos.x - kropp2.pos.x
+    dy = kropp1.pos.y - kropp2.pos.y
+    F_x = G*kropp1.massa*kropp2.massa/dx
+    F_y = G*kropp1.massa*kropp2.massa/dy
+    return Kraft(F_x, F_y)
+
+
+himlakroppar = []
+
+
+def updatera_hastigheter():
+    for kropp in himlakroppar:
+        alla_krafter = []
+        for annan_kropp in himlakroppar:
+            alla_krafter(F(kropp, annan_kropp))
+        F_sum = sum(kraft.x for kraft in alla_krafter) 
+        kropp.hastighet.x += F_sum/kropp.massa*dt
+
+
+def updatera_positioner():
+    for kropp in himlakroppar:
+        kropp.pos.x += kropp.hastighet.x*dt
+        kropp.pos.y += kropp.hastighet.y*dt
